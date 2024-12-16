a7930c7d9b0d90d3491499c25aa97352b43d7bad
belloumi.karim.professional <belloumi.karim.professional@gmail.com>
2024-12-16 15:07:07 +0000

    modify run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}},
+     allow_headers=["Content-Type", "Authorization"],
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
 if __name__ == '__main__':
     app.run(debug=True)
