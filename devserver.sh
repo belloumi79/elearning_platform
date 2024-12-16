b8e27b7b37b6ff39f5fef067630a7d70e86f6dd6
belloumi.karim.professional <belloumi.karim.professional@gmail.com>
2024-12-16 09:37:15 +0000

    modify .gitignore

--- devserver.sh
+++ devserver.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 source myvenv/bin/activate
+export GOOGLE_APPLICATION_CREDENTIALS=/home/user/elearning_platform/config/serviceAccountKey.json
+export PORT=8080
 python -m flask --app /home/user/elearning_platform/run run -p $PORT --debug
